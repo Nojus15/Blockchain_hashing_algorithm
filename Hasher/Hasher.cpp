9379988c19d863e5f7acb597869fb2b1895d2078
Nojus <53999653+Nojus15@users.noreply.github.com>
2022-10-02 18:03:35 +0300

    fixed minor logic issues

--- Hasher/Hasher.cpp
+++ Hasher/Hasher.cpp
@@ -52,30 +52,25 @@ string Hasher::shiftLeft(int posCount, string block)
 
 void Hasher::modifyAddedZeros()
 {
-    //     s0 = (w[i-15] rightrotate 7) xor (w[i-15] rightrotate 18) xor (w[i-15] rightshift 3)
-    //     s1 = (w[i- 2] rightrotate 17) xor (w[i- 2] rightrotate 19) xor (w[i- 2] rightshift 10)
-    //     w[i] = w[i-16] + s0 + w[i-7] + s1
     this->binaryTextStr = this->binaryText.str();
     this->binaryText.str("");
-
-    size_t binaryLenght = binaryTextStr.length();
-    size_t posToStart = binaryLenght - this->addedBits;
 
     std::bitset<32> t1;
     std::bitset<32> t2;
     string s1;
     string s2;
 
-    for (size_t i = posToStart; i < binaryLenght - 1; i += 32)
-    {
-        t1 = ((bitset<32>)this->rotateRight(7, binaryTextStr.substr(i - posToStart, 32)) ^ (bitset<32>)this->rotateRight(18, binaryTextStr.substr(i - posToStart, 32)) ^ (bitset<32>)this->shiftRight(3, binaryTextStr.substr(i - posToStart, 32)));
-        t2 = ((bitset<32>)this->rotateRight(17, binaryTextStr.substr(i - posToStart, 32)) ^ (bitset<32>)this->rotateRight(19, binaryTextStr.substr(i - posToStart, 32)) ^ (bitset<32>)this->shiftRight(10, binaryTextStr.substr(i - posToStart, 32)));
-        t1.flip(2).flip(5).flip(11).flip(17).flip(23).flip(31);
-        t2.flip(3).flip(7).flip(13).flip(19).flip(29).flip(1);
-        s1 = this->addBinary(binaryTextStr.substr(i - posToStart, 32), t1.to_string());
-        s2 = this->addBinary(binaryTextStr.substr(i - posToStart / 2, 32), t2.to_string());
-        this->binaryTextStr.replace(i, 32, this->addBinary(s1, s2));
-    }
+    for (int j = 0; j < 3; j++)
+        for (size_t i = 0; i < binaryTextStr.length(); i += 32)
+        {
+            t1 = ((bitset<32>)this->rotateRight(7, binaryTextStr.substr(i, 32)) ^ (bitset<32>)this->rotateRight(18, binaryTextStr.substr(i, 32)) ^ (bitset<32>)this->shiftRight(3, binaryTextStr.substr(i, 32)));
+            t2 = ((bitset<32>)this->rotateRight(17, binaryTextStr.substr(i, 32)) ^ (bitset<32>)this->rotateRight(19, binaryTextStr.substr(i, 32)) ^ (bitset<32>)this->shiftRight(10, binaryTextStr.substr(i, 32)));
+            t1.flip(2).flip(5).flip(11).flip(17).flip(23).flip(31);
+            t2.flip(3).flip(7).flip(13).flip(19).flip(29).flip(1);
+            s1 = this->addBinary(binaryTextStr.substr(i, 32), t1.to_string());
+            s2 = this->addBinary(binaryTextStr.substr(i / 2, 32), t2.to_string());
+            this->binaryTextStr.replace(i, 32, this->addBinary(s1, s2));
+        }
 };
 string Hasher::addBinary(string b1, string b2)
 {
@@ -99,8 +94,7 @@ string Hasher::addBinary(string b1, string b2)
 void Hasher::convertBinaryToHex()
 {
     string res, tmp;
-    size_t len = this->binaryTextStr.length() / 4;
-    for (int i = this->binaryTextStr.length(); i > this->binaryTextStr.length() - 64 * 4; i -= 4)
+    for (int i = this->binaryTextStr.length(); i > this->binaryTextStr.length() - 64 * 4 - 1; i -= 4)
     {
         tmp = this->binaryTextStr.substr(i, 4);
         if (!tmp.compare("0000"))
@@ -168,8 +162,5 @@ void Hasher::convertBinaryToHex()
             res = res + "f";
         }
     }
-    this->hash = res.substr(0, 64);
+    this->hash = res;
 }
-
-// 61616161617373737373737373730810504EE2E6B79A8FE321B42BCE29825EBD5E310E48E6E3022F7C05BFB8982F0D9010FD048148A284E1913272259E875680EAB9
-// 61616161617373737373737373CBD41412A6AA814FC0D3FD3C801107627D6051DB9B62C22759F2F1B3A178E66E2E3B59BA1709FB7C81AB43551A91D565F680ACD8
